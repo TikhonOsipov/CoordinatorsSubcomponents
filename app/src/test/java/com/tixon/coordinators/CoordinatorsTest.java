610da4e29abe5f98698b80494706c6ba73a83742
tikhon.osipov <tikhon.osipov@hiq.ru>
2016-10-24 11:18:37 +0300

    Test coordinators navigation

--- app/src/test/java/com/tixon/coordinators/CoordinatorsTest.java
+++ app/src/test/java/com/tixon/coordinators/CoordinatorsTest.java
@@ -1,9 +1,5 @@
 package com.tixon.coordinators;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
 import com.tixon.coordinators.mock_views.LoginViewMock;
 import com.tixon.coordinators.mock_views.MainViewMock;
 import com.tixon.coordinators.model.Model;
@@ -14,10 +10,17 @@ import com.tixon.coordinators.screens.login_screen.LoginPresenter;
 import com.tixon.coordinators.screens.main_screen.IMainView;
 import com.tixon.coordinators.screens.main_screen.MainPresenter;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
 /**
  * Created by tikhon.osipov on 22.10.16
  */
 
+/**
+ * Test coordinators navigation
+ */
 @RunWith(JUnit4.class)
 public class CoordinatorsTest {
     private Model model = new Model();
